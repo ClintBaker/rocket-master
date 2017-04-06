71eecc82196b6354e39bedf239ae3b806f3a38cb
Clint Baker <clintonjamesbaker@gmail.com>
2017-04-06 12:02:10 -0600

    add product and category association

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ActiveRecord::Base
   belongs_to :user
+  has_many :product_categories
+  has_many :categories, through: :product_categories
   validates :title, presence: true, length: { minimum: 3, maximum: 50 }
   validates :description, presence: true, length: { minimum: 10, maximum: 300 }
   validates :user_id, presence: true
